90f6ec527ea16525cfb52a5e1966876e1b6462fe
Lutfi Sirajs Demmangago <37861304+lutfisirajs@users.noreply.github.com>
2019-09-30 02:06:37 +0700

    Add files via upload

---
+++ Soal TP Mod 5 WEBPRO/simpleTodoList/assets/js/todos.js
@@ -0,0 +1,23 @@
+$('li').click(function() {
+  if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    });
+  } else {
+    $(this).css({
+      color: 'gray',
+      textDecoration: 'line-through'
+    });
+  }
+});
+$('#semangat').append(
+  "<audio controls autoplay hidden> <source src='../css/semangat.mp3' type='audio/mpeg' /></audio>"
+);
+// lengkapi jquery untuk menlist hasil dari todolist yang di inputkan
+$('ul').on('click', 'li', function() {
+  //  code Here...
+});
+
+// buatlah jquery untuk menekan tombol akan menjadikan data sebagai list
+//  code Here...
